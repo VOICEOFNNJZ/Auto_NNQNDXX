261a2da551199dc55929375dfe3b4f9cdcd83074
DuckBurnIncense <87637270+DuckBurnIncense@users.noreply.github.com>
2022-06-16 18:25:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ headers = {
 def GetStudyID(): # 得到课程id
 	url_get = "http://qndxx.bestcood.com/mp/nanning/my/index.html"
 	response = requests.get(url_get, headers = headers)
-	id = re.findall(r'(?<=\/mp\/nanning\/daxuexi\/detail_)\d+(?=\.html)',response.text)
+	if ('<title>抱歉，出错了</title><meta charset="utf-8">' in response.text):
+		return -1
+	id = re.findall(r'(?<=\/mp\/nanning\/daxuexi\/detail_)\d+(?=\.html)', response.text)
 	return id[0]
 
 def Study(): # 发送post
 	url_hit = "http://qndxx.bestcood.com/mp/nanning/DaXueXi/LearnHit.html"
-	data = {"id": GetStudyID()}
+	id = GetStudyID()
+	if (id == -1): 
+		return -1
+	data = {"id": id}
 	response = requests.post(url_hit, headers = headers, data = data)
 	return int(json.loads(response.text)['code'])
 
